639198b14058511288bbba0cba444aa1d68fb6c1
Karistin Simbolon <karistinsimbolon@gmail.com>
2026-04-01 11:10:17 +0700

    menambahkan produk

---
+++ src/main/java/com/example/productcrud/service/ProductService.java
@@ -0,0 +1,33 @@
+package com.example.productcrud.service;
+
+import com.example.productcrud.model.Product;
+import com.example.productcrud.repository.ProductRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
